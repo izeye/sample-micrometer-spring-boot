ef37111a37d6e8873fbb86ce2d2b8af73401355f
Johnny Lim <izeye@naver.com>
2018-10-10 08:26:29 +0900

    Polish

--- src/test/java/com/izeye/sample/InfluxMeterRegistryTests.java
+++ src/test/java/com/izeye/sample/InfluxMeterRegistryTests.java
@@ -20,35 +20,34 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class InfluxMeterRegistryTests {
 
+	private final InfluxConfig config = new InfluxConfig() {
+
+		@Override
+		public String get(String key) {
+			return null;
+		}
+
+		@Override
+		public Duration step() {
+			return Duration.ofSeconds(5);
+		}
+
+	};
+
+	private final InfluxMeterRegistry registry = new InfluxMeterRegistry(this.config, Clock.SYSTEM);
+
 	@Test
-	public void test() throws InterruptedException {
-		InfluxConfig config = new InfluxConfig() {
-
-			@Override
-			public String get(String key) {
-				return null;
-			}
-
-			@Override
-			public Duration step() {
-				return Duration.ofSeconds(5);
-			}
-
-		};
-
-		InfluxMeterRegistry registry = new InfluxMeterRegistry(config, Clock.SYSTEM);
-
-		Counter counter = registry.counter("name", Tags.of("key", "value"));
+	public void counter() throws InterruptedException {
+		Counter counter = this.registry.counter("name", Tags.of("key", "value"));
 		assertThat(counter).isExactlyInstanceOf(StepCounter.class);
+
 		counter.increment();
 		assertThat(counter.count()).isEqualTo(0);
 
 		TimeUnit.SECONDS.sleep(5);
-
 		assertThat(counter.count()).isEqualTo(1);
 
 		TimeUnit.SECONDS.sleep(5);
-
 		assertThat(counter.count()).isEqualTo(0);
 	}
 
