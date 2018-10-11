5f3ac5aa6f3b15dd406cd21456f4fa7053f3e967
Johnny Lim <izeye@naver.com>
2018-10-11 14:10:57 +0900

    Add a broken test
    See https://github.com/micrometer-metrics/micrometer/issues/911

---
+++ src/test/java/com/izeye/sample/ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.izeye.sample;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests for {@link Application}.
+ *
+ * @author Johnny Lim
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void test() {
+	}
+
+}
