5ebfa40f8a78369ba583bd00ce3f0c31dbb155bb
Johnny Lim <izeye@naver.com>
2018-12-27 01:11:53 +0900

    Add a sample meter filter for adding tags to specific meters
    See https://stackoverflow.com/questions/53925641/micrometer-common-tags-for-specific-metrics

--- src/main/java/com/izeye/sample/config/MetricsConfig.java
+++ src/main/java/com/izeye/sample/config/MetricsConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
 
@@ -32,6 +35,20 @@ public class MetricsConfig {
 			}
 
 		}, Clock.SYSTEM);
+	}
+
+	@Bean
+	public MeterFilter meterFilter() {
+		return new MeterFilter() {
+
+			@Override
+			public Meter.Id map(Meter.Id id) {
+				return id.getName().startsWith("logback")
+						? id.withTags(Tags.concat(Tags.of("key1", "value1"), id.getTagsAsIterable()))
+						: id;
+			}
+
+		};
 	}
 
 }
