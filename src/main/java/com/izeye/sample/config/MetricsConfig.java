e806d3d62f469047ede07f6ae6de364ed8c8c739
Johnny Lim <izeye@naver.com>
2018-12-27 01:02:12 +0900

    Set step for logging meter registry

--- src/main/java/com/izeye/sample/config/MetricsConfig.java
+++ src/main/java/com/izeye/sample/config/MetricsConfig.java
@@ -1,9 +1,13 @@
 package com.izeye.sample.config;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
+import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
 
 /**
  * Configuration for metrics.
@@ -15,7 +19,19 @@ public class MetricsConfig {
 
 	@Bean
 	public LoggingMeterRegistry loggingMeterRegistry() {
-		return new LoggingMeterRegistry();
+		return new LoggingMeterRegistry(new LoggingRegistryConfig() {
+
+			@Override
+			public String get(String key) {
+				return null;
+			}
+
+			@Override
+			public Duration step() {
+				return Duration.ofSeconds(5);
+			}
+
+		}, Clock.SYSTEM);
 	}
 
 }
