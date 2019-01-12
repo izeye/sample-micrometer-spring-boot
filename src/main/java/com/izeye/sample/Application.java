7be280297eeb79e40a3d705a03388f3433f966c1
Johnny Lim <izeye@naver.com>
2019-01-12 10:33:51 +0900

    Add tests for MBean object name patterns
    See https://github.com/micrometer-metrics/micrometer/issues/1145

--- src/main/java/com/izeye/sample/Application.java
+++ src/main/java/com/izeye/sample/Application.java
@@ -1,5 +1,14 @@
 package com.izeye.sample;
 
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,6 +22,30 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@PostConstruct
+	public void initialize() {
+		MBeanServer mBeanServer = getMBeanServer();
+		try {
+			String objectNamePattern = "Tomcat:type=ThreadPool,*";
+			System.out.println("# Object name pattern: " + objectNamePattern);
+			mBeanServer.queryNames(new ObjectName(objectNamePattern), null).forEach(System.out::println);
+
+			objectNamePattern = "Tomcat:type=ThreadPool,name=*";
+			System.out.println("# Object name pattern: " + objectNamePattern);
+			mBeanServer.queryNames(new ObjectName(objectNamePattern), null).forEach(System.out::println);
+		} catch (MalformedObjectNameException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public static MBeanServer getMBeanServer() {
+		List<MBeanServer> mBeanServers = MBeanServerFactory.findMBeanServer(null);
+		if (!mBeanServers.isEmpty()) {
+			return mBeanServers.get(0);
+		}
+		return ManagementFactory.getPlatformMBeanServer();
 	}
 
 }
