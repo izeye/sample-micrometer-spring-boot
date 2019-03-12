19a87b7215db5a3e973920826fcae07ecd5bf2d4
Johnny Lim <izeye@naver.com>
2019-03-12 22:58:34 +0900

    Configure Caffeine cache

--- src/main/java/com/izeye/sample/web/SampleController.java
+++ src/main/java/com/izeye/sample/web/SampleController.java
@@ -3,6 +3,7 @@ package com.izeye.sample.web;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class SampleController {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Cacheable("my-cache")
 	@GetMapping("/call-rest-template")
 	public Map<String, Object> callRestTemplate() {
 		return this.restTemplate.getForObject("https://spring.io/info", Map.class);
