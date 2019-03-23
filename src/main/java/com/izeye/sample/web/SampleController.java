52c9657ab17a201c056f71a8a82cc9716683c30d
Johnny Lim <izeye@naver.com>
2019-03-23 22:09:20 +0900

    Add a sample for async request with DeferredResult

--- src/main/java/com/izeye/sample/web/SampleController.java
+++ src/main/java/com/izeye/sample/web/SampleController.java
@@ -1,12 +1,14 @@
 package com.izeye.sample.web;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
 
 /**
  * Sample {@link RestController}.
@@ -23,6 +25,24 @@ public class SampleController {
 	@GetMapping("/call-rest-template")
 	public Map<String, Object> callRestTemplate() {
 		return this.restTemplate.getForObject("https://spring.io/info", Map.class);
+	}
+
+	@GetMapping("/quotes")
+	public DeferredResult<String> quotes() {
+		DeferredResult<String> deferredResult = new DeferredResult<>();
+		process(deferredResult);
+		return deferredResult;
+	}
+
+	private void process(DeferredResult<String> deferredResult) {
+		new Thread(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(5);
+				deferredResult.setResult("Hello, world!");
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+		}).start();
 	}
 
 }
