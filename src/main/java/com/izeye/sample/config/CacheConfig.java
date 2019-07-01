440a3a6586623e37bc75b5f910fce7cc5ddbd4c2
Johnny Lim <izeye@naver.com>
2019-07-01 15:58:58 +0900

    Enable cache

---
+++ src/main/java/com/izeye/sample/config/CacheConfig.java
@@ -0,0 +1,14 @@
+package com.izeye.sample.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for cache.
+ *
+ * @author Johnny Lim
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
