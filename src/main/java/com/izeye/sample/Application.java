525e154830ecdbabc076ad5bcca7b9497cab52c0
Johnny Lim <izeye@naver.com>
2019-07-22 23:05:43 +0900

    Provide custom WebFluxTagsProvider
    See https://github.com/micrometer-metrics/micrometer/issues/1510

--- src/main/java/com/izeye/sample/Application.java
+++ src/main/java/com/izeye/sample/Application.java
@@ -1,10 +1,17 @@
 package com.izeye.sample;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.web.reactive.server.WebFluxTags;
+import org.springframework.boot.actuate.metrics.web.reactive.server.WebFluxTagsProvider;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+
+import io.micrometer.core.instrument.Tag;
 
 /**
  * Sample application.
@@ -23,6 +30,16 @@ public class Application {
 				.route(p -> p.path("/status/304")
 						.uri("http://httpbin.org:80/status/304"))
 				.build();
+	}
+
+	@Bean
+	public WebFluxTagsProvider webFluxTagsProvider() {
+		return (exchange, exception) -> Arrays.asList(WebFluxTags.method(exchange), getUri(exchange),
+				WebFluxTags.exception(exception), WebFluxTags.status(exchange), WebFluxTags.outcome(exchange));
+	}
+
+	private Tag getUri(ServerWebExchange exchange) {
+		return Tag.of("uri", exchange.getRequest().getURI().toString());
 	}
 
 	public static void main(String[] args) {
