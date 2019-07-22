3bb8e1328a94dc05f035cc46b9752850b1a79a39
Johnny Lim <izeye@naver.com>
2019-07-22 22:19:15 +0900

    Add a sample for Spring Cloud Gateway

--- src/main/java/com/izeye/sample/Application.java
+++ src/main/java/com/izeye/sample/Application.java
@@ -2,6 +2,9 @@ package com.izeye.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Sample application.
@@ -10,6 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80/get"))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
