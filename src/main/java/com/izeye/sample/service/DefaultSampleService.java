5da2abdc88760324e19899195b69abe2af436cb8
Johnny Lim <izeye@naver.com>
2019-10-23 19:11:00 +0900

    Add a sample timer

--- src/main/java/com/izeye/sample/service/DefaultSampleService.java
+++ src/main/java/com/izeye/sample/service/DefaultSampleService.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 
 /**
  * Default {@link SampleService}.
@@ -19,10 +20,16 @@ public class DefaultSampleService implements SampleService {
 
 	private final RestTemplate restTemplate;
 
+	private final Timer timer;
+
 	private final DistributionSummary distributionSummary;
 
 	public DefaultSampleService(RestTemplate restTemplate, MeterRegistry meterRegistry) {
 		this.restTemplate = restTemplate;
+
+		this.timer = Timer.builder("sample.time")
+				.publishPercentiles(0.5, 0.75, 0.95, 0.99, 0.999)
+				.register(meterRegistry);
 
 		this.distributionSummary = DistributionSummary.builder("sample.value")
 				.publishPercentiles(0.5, 0.75, 0.95, 0.99, 0.999)
@@ -33,7 +40,13 @@ public class DefaultSampleService implements SampleService {
 	public Map<String, Object> doService() {
 		this.distributionSummary.record(ThreadLocalRandom.current().nextDouble() * 100);
 
-		return this.restTemplate.getForObject("https://spring.io/info", Map.class);
+		Timer.Sample sample = Timer.start();
+		try {
+			return this.restTemplate.getForObject("https://spring.io/info", Map.class);
+		}
+		finally {
+			sample.stop(this.timer);
+		}
 	}
 
 }
