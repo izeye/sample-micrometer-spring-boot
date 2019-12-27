a67edc8a8be9842e594383041967207dacd4dfec
Johnny Lim <izeye@naver.com>
2019-12-28 06:07:52 +0900

    Demonstrate how to customize DistributionStatisticConfig via MeterFilter
    See https://github.com/micrometer-metrics/micrometer/issues/1769

--- src/main/java/com/izeye/sample/config/MetricsConfig.java
+++ src/main/java/com/izeye/sample/config/MetricsConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
 
@@ -19,7 +22,7 @@ public class MetricsConfig {
 
 	@Bean
 	public LoggingMeterRegistry loggingMeterRegistry() {
-		return new LoggingMeterRegistry(new LoggingRegistryConfig() {
+		LoggingMeterRegistry loggingMeterRegistry = new LoggingMeterRegistry(new LoggingRegistryConfig() {
 
 			@Override
 			public String get(String key) {
@@ -32,6 +35,14 @@ public class MetricsConfig {
 			}
 
 		}, Clock.SYSTEM);
+
+		loggingMeterRegistry.config().meterFilter(new MeterFilter() {
+			@Override
+			public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+				return DistributionStatisticConfig.builder().expiry(Duration.ofHours(1)).build().merge(config);
+			}
+		});
+		return loggingMeterRegistry;
 	}
 
 }
