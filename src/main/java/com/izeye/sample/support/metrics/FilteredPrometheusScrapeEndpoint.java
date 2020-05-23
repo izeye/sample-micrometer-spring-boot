36b464917f607d0087fdab1fdd2dcb7beebab384
Johnny Lim <izeye@naver.com>
2020-05-23 12:48:16 +0900

    Support parametric metrics filtering in Prometheus scrape endpoint

--- src/main/java/com/izeye/sample/support/metrics/FilteredPrometheusScrapeEndpoint.java
+++ src/main/java/com/izeye/sample/support/metrics/FilteredPrometheusScrapeEndpoint.java
@@ -3,35 +3,38 @@ package com.izeye.sample.support.metrics;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Set;
 
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.common.TextFormat;
 
 /**
- * Custom {@link org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint}.
+ * Filter-supported {@link org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint}.
  *
  * @author Johnny Lim
  */
 @Component
-@WebEndpoint(id = "customPrometheus")
-public class CustomPrometheusScrapeEndpoint {
+@WebEndpoint(id = "filtered-prometheus")
+public class FilteredPrometheusScrapeEndpoint  {
 
 	private final CollectorRegistry collectorRegistry;
 
-	public CustomPrometheusScrapeEndpoint(CollectorRegistry collectorRegistry) {
+	public FilteredPrometheusScrapeEndpoint(CollectorRegistry collectorRegistry) {
 		this.collectorRegistry = collectorRegistry;
 	}
 
 	@ReadOperation(produces = TextFormat.CONTENT_TYPE_004)
-	public String scrape() {
+	public String scrape(@Nullable Set<String> includedNames) {
 		try {
 			Writer writer = new StringWriter();
-			TextFormat.write004(writer, this.collectorRegistry.filteredMetricFamilySamples(Collections.singleton("jvm_gc_max_data_size_bytes")));
+			TextFormat.write004(writer, getSamples(includedNames));
 			return writer.toString();
 		}
 		catch (IOException ex) {
@@ -39,6 +42,13 @@ public class CustomPrometheusScrapeEndpoint {
 			// IOException
 			throw new RuntimeException("Writing metrics failed", ex);
 		}
+	}
+
+	private Enumeration<Collector.MetricFamilySamples> getSamples(@Nullable Set<String> includedNames) {
+		if (includedNames == null) {
+			return this.collectorRegistry.metricFamilySamples();
+		}
+		return this.collectorRegistry.filteredMetricFamilySamples(includedNames);
 	}
 
 }
