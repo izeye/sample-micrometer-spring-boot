d2703ecbb1230132c4887a69156f4aa4c9cb8963
Johnny Lim <izeye@naver.com>
2020-10-10 13:18:58 +0900

    Polish

--- src/main/java/com/izeye/sample/web/SampleController.java
+++ src/main/java/com/izeye/sample/web/SampleController.java
@@ -2,7 +2,9 @@ package com.izeye.sample.web;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +19,20 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping(path = "/sample")
 public class SampleController {
 
-	@Autowired
-	private RestTemplate restTemplate;
+	private final RestTemplate restTemplate;
+
+	public SampleController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
 	@GetMapping("/call-rest-template")
 	public Map<String, Object> callRestTemplate() {
-		return this.restTemplate.getForObject("https://spring.io/info", Map.class);
+		ResponseEntity<Map<String, Object>> response = this.restTemplate.exchange(
+				"https://spring.io/info",
+				HttpMethod.GET,
+				null,
+				new ParameterizedTypeReference<Map<String, Object>>() {});
+		return response.getBody();
 	}
 
 }
