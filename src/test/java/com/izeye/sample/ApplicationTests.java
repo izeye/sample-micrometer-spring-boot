271aa1f52bdd4d11c43addeefd78bee5e6b278bb
Johnny Lim <izeye@naver.com>
2020-10-10 13:27:23 +0900

    Add a test for application context loading

---
+++ src/test/java/com/izeye/sample/ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.izeye.sample;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link Application}.
+ *
+ * @author Johnny Lim
+ */
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void test() {
+	}
+
+}
