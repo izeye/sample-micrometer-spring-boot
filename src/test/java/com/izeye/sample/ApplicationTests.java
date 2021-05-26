56473e48116a0931a285e794626a18ad67f46be0
izeye <izeye@naver.com>
2021-05-26 18:05:43 +0900

    Add a basic Spring Boot integration test

---
+++ src/test/java/com/izeye/sample/ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.izeye.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Tests for {@link Application}.
+ *
+ * @author Johnny Lim
+ */
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
